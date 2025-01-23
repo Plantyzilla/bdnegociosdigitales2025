7c36b8884104099d03d14341c5f1579865b8a08f
Plantyzilla123 <yaeltolentinoosornio@gmail.com>
2025-01-23 17:38:19 -0600

    Logro: Ejercicio de SQL-LDD terminado

--- 02_Creacion_llenado_Una_Tabla.sql
+++ 02_Creacion_llenado_Una_Tabla.sql
@@ -39,6 +39,31 @@ values ('Panaderia', 7),
         ('Jugueteria', 9);
         
 insert into categoria
-values (10, 'Panaderia');
+values (10, 'Arina');
+
+#Tabla productos
+create table productos1 (
+productoid int not null,
+nombreProducto varchar (20) not null,
+descripcion varchar (80),
+#Aqui no existe "money" aqui es "decimal (10,2)"
+precio decimal (10,2),
+existencia int not null,
+categoriaid int null,
+constraint pk_producto1
+primary key (productoid),
+constraint unico_descripcion
+unique (nombreProducto),
+constraint chk_precio
+check (precio>0.0 and precio<=1000),
+constraint chk_existencia
+check (existencia>0 and existencia <=200),
+constraint fk_categoria_productos1
+foreign key (categoriaid)
+references categoria (categoriaid)
+);
+insert into producto1
+values (1, 'Miguelito','Dulce sano para la lombriz', 34.5,45,5);
+
         
 select * from categoria;
